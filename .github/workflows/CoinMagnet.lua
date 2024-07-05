f5b5c4baed50f61e324a3d6dc9469ce543211b39
Swordsartonline <88285313+Swordsartonline@users.noreply.github.com>
2024-07-06 01:56:18 +0800

    Update CoinMagnet.lua

--- .github/workflows/CoinMagnet.lua
+++ .github/workflows/CoinMagnet.lua
@@ -1,52 +1,67 @@
--- UI Library
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/weakhoes/Roblox-UI-Libs/main/Discord%20UI%20Lib/Discord%20Lib%20Source.lua"))()
+local discord = loadstring(game:HttpGet("https://raw.githubusercontent.com/weakhoes/Roblox-UI-Libs/main/Discord%20UI%20Lib/Discord%20Lib%20Source.lua"))()
 
--- Game variables
-local player = game.Players.LocalPlayer
-local mouse = player:GetMouse()
-local character = player.Character
-local humanoid = character:WaitForChild("Humanoid")
-local magnet = game:GetService("ReplicatedStorage").Magnet
+local player = game:GetService("Players").LocalPlayer
+local replication = game:GetService("ReplicatedStorage").Remotes.Replication
+local coins = player:WaitForChild("Stats").Coins
+local gems = player:WaitForChild("Stats").Gems
 
--- UI variables
-local ui = Library.new("Magnet Simulator")
-local settings = ui:AddPage("Settings")
-local toggle = settings:AddToggle("Magnet Toggle", false)
-
--- Function to get all coins in range
-local function getCoins()
-    local coins = workspace:GetChildren()
-    local coinsInRange = {}
-
-    for i, coin in pairs(coins) do
-        if coin:FindFirstChild("CoinValue") and coin.Position - character.Torso.Position.Magnitude < 20 then
-            table.insert(coinsInRange, coin)
-        end
-    end
-
-    return coinsInRange
+local function updateUI()
+    replication:InvokeServer("UpdateUI")
 end
 
--- Event for when the magnet toggle is changed
-toggle.Changed:Connect(function(value)
-    if value then
-        humanoid.WalkSpeed = 0
-        local coins = getCoins()
+local function addCurrency(coinAmount, gemAmount)
+    coins.Value = coins.Value + coinAmount
+    gems.Value = gems.Value + gemAmount
+    updateUI()
+end
 
-        for i, coin in pairs(coins) do
-            magnet:FireServer(coin)
-        end
+local window = discord.Window:new({
+    Title = "Coin Magnet Simulator Cheats";
+    Size = {300, 300};
+    Resizable = false;
+    MinSize = {300, 300};
+    Maximizable = false;
+    TransparentFromDock = true;
+})
 
-        wait(1)
-        humanoid.WalkSpeed = 16
+local coinInput = discord.Input:new({
+    Parent = window.Content;
+    PlaceholderText = "Coin Amount";
+    Size = UDim2.new(1, -20, 0, 25);
+    Position = UDim2.new(0, 10, 0, 10);
+})
+
+local gemInput = discord.Input:new({
+    Parent = window.Content;
+    PlaceholderText = "Gem Amount";
+    Size = UDim2.new(1, -20, 0, 25);
+    Position = UDim2.new(0, 10, 0, 40);
+})
+
+local addButton = discord.Button:new({
+    Parent = window.Content;
+    Text = "Add Currency";
+    Size = UDim2.new(1, -20, 0, 25);
+    Position = UDim2.new(0, 10, 0, 70);
+    OnClick = function()
+        local coinAmount = tonumber(coinInput.Text) or 0
+        local gemAmount = tonumber(gemInput.Text) or 0
+        addCurrency(coinAmount, gemAmount)
+    end;
+})
+
+local function toggleUI()
+    if window:IsVisible() then
+        window:Hide()
+    else
+        window:Show()
+    end
+end
+
+game:GetService("UserInputService").Keyboard.KeyDown:Connect(function(key)
+    if key:ToLower() == "insert" then
+        toggleUI()
     end
 end)
 
--- Event for when the player clicks the left mouse button
-mouse.Button1Down:Connect(function()
-    local coins = getCoins()
-
-    for i, coin in pairs(coins) do
-        magnet:FireServer(coin)
-    end
-end)
+addCurrency(1000000, 1000000)
